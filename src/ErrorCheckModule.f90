4b4f65d0fdc62cc7de6b223d202752d97075976c
Naoki Mizukami <mizukami@ucar.edu>
2022-08-15 09:08:50 -0600

    Added values bound check function and some descriptions

--- src/ErrorCheckModule.f90
+++ src/ErrorCheckModule.f90
@@ -1,13 +1,18 @@
 module ErrorCheckModule
 
+  ! General error checking routins
+
   implicit none
 
   private
   public:: sys_abort
+  public:: is_within_bound
 
 contains
 
   subroutine sys_abort(err, message)
+
+    ! terminate the program if error is detected (err is non-zero)
 
     implicit none
 
@@ -21,5 +26,24 @@ contains
     endif
 
   end subroutine sys_abort
+
+
+  function is_within_bound(lower_bound, upper_bound, var) result(withinbound)
+
+    ! check if a value is within specified bounds
+
+    implicit none
+
+    integer, intent(in) :: lower_bound
+    integer, intent(in) :: upper_bound
+    integer, intent(in) :: var
+    logical :: withinbound
+
+    withinbound = .true.
+    if ( var < lower_bound .or. var > upper_bound ) then
+       withinbound = .false.
+    end if
+
+  end function
 
 end module ErrorCheckModule
