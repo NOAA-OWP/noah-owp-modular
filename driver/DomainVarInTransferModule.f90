9ece0d0a3488537210624e4c295e667f38e99632
GreyEvenson-NOAA <grey.evenson@noaa.gov>
2023-06-09 12:32:45 -0400

    Initial write of InTransfer subroutines for domain_type, energy_type, and parameters_type

--- driver/DomainVarInTransferModule.f90
+++ driver/DomainVarInTransferModule.f90
@@ -1,7 +1,7 @@
 module DomainVarInTransferModule
 
-    use NoahowpmpIOVarType
-    use NoahowpType
+    use NoahowpmpIOType
+    use NoahowpmpType
 
     implicit none
   
@@ -11,37 +11,37 @@ module DomainVarInTransferModule
 
     implicit none
 
-    type(NoahmpIO_type), intent(inout) :: NoahowpmpIO
-    type(noahmp_type),   intent(inout) :: noahowpmp
+    type(NoahowpmpIO_type), intent(inout) :: NoahowpmpIO
+    type(noahowp_type),     intent(inout) :: noahowpmp
 
     associate(ix   => NoahowpmpIO%ix, &
               iy   => NoahowpmpIO%iy)
 
-    noahowpmp%DT = NoahowpmpIO%DT
-    noahowpmp%startdate = NoahowpmpIO%startdate 
-    noahowpmp%enddate = NoahowpmpIO%enddate    
-    noahowpmp%nowdate = NoahowpmpIO%nowdate           
-    noahowpmp%start_datetime = NoahowpmpIO%start_datetime  
-    noahowpmp%end_datetime = NoahowpmpIO%end_datetime  
-    noahowpmp%curr_datetime = NoahowpmpIO%curr_datetime    
-    noahowpmp%sim_datetimes = NoahowpmpIO%sim_datetimes
-    noahowpmp%itime = NoahowpmpIO%itime          
-    noahowpmp%ntime = NoahowpmpIO%ntime          
-    noahowpmp%time_dbl = NoahowpmpIO%time_dbl
-    noahowpmp%ix = ix 
-    noahowpmp%iy = iy 
-    noahowpmp%lat = NoahowpmpIO%lat(ix,iy)       
-    noahowpmp%lon = NoahowpmpIO%lon(ix,iy)       
-    noahowpmp%ZREF = NoahowpmpIO%ZREF(ix,iy)         
-    noahowpmp%terrain_slope = NoahowpmpIO%terrain_slope(ix,iy) 
-    noahowpmp%azimuth = NoahowpmpIO%azimuth(ix,iy)          
-    noahowpmp%vegtyp = NoahowpmpIO%vegtyp(ix,iy)    
-    noahowpmp%croptype = NoahowpmpIO%croptype(ix,iy)        
-    noahowpmp%isltyp = NoahowpmpIO%isltyp(ix,iy)       
-    noahowpmp%IST = NoahowpmpIO%IST(ix,iy)      
-    noahowpmp%zsoil(:) = NoahowpmpIO%zsoil(ix,iy,:)    
-    noahowpmp%dzsnso(:) = NoahowpmpIO%dzsnso(ix,iy,:)   
-    noahowpmp%zsnso(:) = NoahowpmpIO%zsnso(ix,iy,:)    
+    noahowpmp%domain%DT = NoahowpmpIO%DT
+    noahowpmp%domain%startdate = NoahowpmpIO%startdate 
+    noahowpmp%domain%enddate = NoahowpmpIO%enddate    
+    noahowpmp%domain%nowdate = NoahowpmpIO%nowdate           
+    noahowpmp%domain%start_datetime = NoahowpmpIO%start_datetime  
+    noahowpmp%domain%end_datetime = NoahowpmpIO%end_datetime  
+    noahowpmp%domain%curr_datetime = NoahowpmpIO%curr_datetime    
+    noahowpmp%domain%sim_datetimes = NoahowpmpIO%sim_datetimes
+    noahowpmp%domain%itime = NoahowpmpIO%itime          
+    noahowpmp%domain%ntime = NoahowpmpIO%ntime          
+    noahowpmp%domain%time_dbl = NoahowpmpIO%time_dbl
+    noahowpmp%domain%iloc = ix 
+    noahowpmp%domain%jloc = iy 
+    noahowpmp%domain%lat = NoahowpmpIO%lat(ix,iy)       
+    noahowpmp%domain%lon = NoahowpmpIO%lon(ix,iy)       
+    noahowpmp%domain%ZREF = NoahowpmpIO%ZREF(ix,iy)         
+    noahowpmp%domain%terrain_slope = NoahowpmpIO%terrain_slope(ix,iy) 
+    noahowpmp%domain%azimuth = NoahowpmpIO%azimuth(ix,iy)          
+    noahowpmp%domain%vegtyp = NoahowpmpIO%vegtyp(ix,iy)    
+    noahowpmp%domain%croptype = NoahowpmpIO%croptype(ix,iy)        
+    noahowpmp%domain%isltyp = NoahowpmpIO%isltyp(ix,iy)       
+    noahowpmp%domain%IST = NoahowpmpIO%IST(ix,iy)      
+    noahowpmp%domain%zsoil(:) = NoahowpmpIO%zsoil(ix,iy,:)    
+    noahowpmp%domain%dzsnso(:) = NoahowpmpIO%dzsnso(ix,iy,:)   
+    noahowpmp%domain%zsnso(:) = NoahowpmpIO%zsnso(ix,iy,:)    
 
     end associate
 
