e433100152ef93d88d383a115e0d62cb87f01f95
GreyEvenson-NOAA <grey.evenson@noaa.gov>
2023-06-29 12:20:43 -0400

    Moved subroutine to iterate over noahowpgrid_type to RunModule.f90 and renamed to 'solve_noahowp_grid'; readded advance_in_time and cleanup subroutines

--- src/RunModule.f90
+++ src/RunModule.f90
@@ -23,7 +23,108 @@ module RunModule
 
 contains
   
+
+  SUBROUTINE cleanup()
+    implicit none
+      
+    !---------------------------------------------------------------------
+    ! Compiler directive NGEN_OUTPUT_ACTIVE to be defined if 
+    ! Nextgen is writing model output (https://github.com/NOAA-OWP/ngen)
+    !---------------------------------------------------------------------
+#ifndef NGEN_OUTPUT_ACTIVE
+    call finalize_output()
+#endif
+    
+  END SUBROUTINE cleanup
+
+  SUBROUTINE advance_in_time(noahowpgrid)
+    type (noahowpgrid_type), intent (inout) :: noahowpgrid
+
+    call solve_noahowp_grid(noahowpgrid)
+
+    model%domain%itime    = model%domain%itime + 1 ! increment the integer time by 1
+    model%domain%time_dbl = dble(model%domain%time_dbl + model%domain%dt) ! increment model time in seconds by DT
+
+  END SUBROUTINE advance_in_time
+
+  SUBROUTINE solve_noahowp_grid(noahowpgrid)
+
+    implicit none
+    type(noahowpgrid_type), intent(inout) :: noahowpgrid
+    type(noahowp_type)                    :: noahowp                                             !local instance 
+    integer                               :: ix, iy, ierr
+    integer                               :: iunit = 10
+    real                                  :: read_UU, read_VV, read_SFCTMP, read_Q2, read_SFCPRS !to read in forcing
+    real                                  :: read_SOLDN, read_LWDN, read_PRCP                    !to read in forcing
+    integer                               :: idt                                                 !to iterate nowdate
+
+  #ifndef NGEN_FORCING_ACTIVE
+
+    !ifndef NGEN_FORCING_ACTIVE, read forcings for time step, else then 
+    !forcings must be updated/set via bmi function calls prior to call 
+    !to bmi_update/NoahowpGridDriverMain
+
+    !Update noahowpgrid%nowdate 
+    !I think this needs to be done here rather than in UtilitiesMain if NGEN isn't doing forcing
+    !idt = noahowpgrid%itime * (noahowpgrid%dt / 60)
+    !call geth_newdate(noahowpgrid%startdate, idt, noahowpgrid%nowdate)
+
+    !Read for forcings for nowdate
+    call read_forcing_text(iunit, noahowpgrid%nowdate, int(noahowpgrid%dt), &
+          read_UU, read_VV, read_SFCTMP, read_Q2, read_SFCPRS, read_SOLDN, read_LWDN, read_PRCP, ierr)
+
+    !Give read-in forcings to all grid cells
+    noahowpgrid%UU(:,:)     = read_UU
+    noahowpgrid%VV(:,:)     = read_VV
+    noahowpgrid%SFCTMP(:,:) = read_SFCTMP
+    noahowpgrid%Q2(:,:)     = read_Q2
+    noahowpgrid%SFCPRS(:,:) = read_SFCPRS
+    noahowpgrid%SOLDN(:,:)  = read_SOLDN
+    noahowpgrid%LWDN(:,:)   = read_LWDN
+    noahowpgrid%PRCP(:,:)   = read_PRCP
+    noahowpgrid%UU(:,:)     = read_UU
+
+  #endif
+      
+    !Initialize local instance of noahowp_type (noahowpgrid is needed to set z dim length. Or we could just pass nsoil and nsnow)
+    call noahowp%Init(noahowpgrid)
+
+    !Iterate over x and y dimensions
+    do ix = 1, noahowpgrid%n_x
+      do iy = 1, noahowpgrid%n_y
+
+        !Give ix and iy to noahowp_type
+        noahowpgrid%ix = ix
+        noahowpgrid%iy = iy
+
+        !Transfer variable values from noahowpgrid_type to noahowp_type
+        call DomainVarInTransfer       (noahowp, noahowpgrid)
+        call LevelsVarInTransfer       (noahowp, noahowpgrid)
+        call EnergyVarInTransfer       (noahowp, noahowpgrid)
+        call ForcingVarInTransfer      (noahowp, noahowpgrid)
+        call OptionsVarInTransfer      (noahowp, noahowpgrid)
+        call ParametersVarInTransfer   (noahowp, noahowpgrid)
+        call WaterVarInTransfer        (noahowp, noahowpgrid)
+        
+        !Execute column model
+        call solve_noahowp             (noahowp)
+
+        !Transfer variable values from noahowp_type back to noahowpgrid_type
+        call DomainVarOutTransfer      (noahowp, noahowpgrid)
+        call LevelsVarOutTransfer      (noahowp, noahowpgrid)
+        call EnergyVarOutTransfer      (noahowp, noahowpgrid)
+        call ForcingVarOutTransfer     (noahowp, noahowpgrid)
+        call OptionsVarOutTransfer     (noahowp, noahowpgrid)
+        call ParametersVarOutTransfer  (noahowp, noahowpgrid)
+        call WaterVarOutTransfer       (noahowp, noahowpgrid)
+
+      end do
+    end do
+
+  END SUBROUTINE solve_noahowp_grid
+
   SUBROUTINE solve_noahowp(noahowp)
+
     type (noahowp_type), intent (inout) :: noahowp
     integer, parameter                  :: iunit        = 10 ! Fortran unit number to attach to the opened file
     integer                             :: forcing_timestep  ! integer time step (set to dt) for some subroutine calls
@@ -74,5 +175,19 @@ contains
     
     end associate ! terminate associate block
   END SUBROUTINE solve_noahowp
+
+  SUBROUTINE cleanup()
+    implicit none
+      
+      !---------------------------------------------------------------------
+      ! Compiler directive NGEN_OUTPUT_ACTIVE to be defined if 
+      ! Nextgen is writing model output (https://github.com/NOAA-OWP/ngen)
+      !---------------------------------------------------------------------
+  
+#ifndef NGEN_OUTPUT_ACTIVE
+      call finalize_output()
+#endif
+    
+  END SUBROUTINE cleanup
 
 end module RunModule
