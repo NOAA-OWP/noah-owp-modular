12f9228c3bf02d8bfe3897a6470a26dc1b51fc4a
GreyEvenson-NOAA <grey.evenson@noaa.gov>
2023-07-03 09:26:29 -0400

    Added new optionsgrid_type

--- src/OptionsTypeTransfer.f90
+++ src/OptionsTypeTransfer.f90
@@ -1,73 +1,51 @@
 module OptionsTypeTransfer
 
-    use NoahowpGridTypeModule
-    use NoahowpType
+    use OptionsType
+    use OptionsGridType
 
     implicit none
   
   contains
   
-  subroutine OptionsVarInTransfer(noahowp, noahowpgrid)
+  subroutine OptionsVarInTransfer(options, optionsgrid, ix, iy)
 
     implicit none
 
-    type(noahowpgrid_type), intent(in)    :: noahowpgrid
-    type(noahowp_type),     intent(inout) :: noahowp
-
-    associate(ix   => noahowpgrid%ix, &
-              iy   => noahowpgrid%iy)
+    type(options_type),         intent(inout)    :: options
+    type(optionsgrid_type),     intent(in)       :: optionsgrid
+    integer,                    intent(in)       :: ix
+    integer,                    intent(in)       :: iy
   
-    noahowp%options%opt_snf   = noahowpgrid%opt_snf(ix,iy)
-    noahowp%options%opt_run   = noahowpgrid%opt_run(ix,iy)
-    noahowp%options%opt_drn   = noahowpgrid%opt_drn(ix,iy)
-    noahowp%options%opt_inf   = noahowpgrid%opt_inf(ix,iy)
-    noahowp%options%opt_infdv = noahowpgrid%opt_infdv(ix,iy)
-    noahowp%options%dveg      = noahowpgrid%dveg(ix,iy)
-    noahowp%options%opt_alb   = noahowpgrid%opt_alb(ix,iy)
-    noahowp%options%opt_rad   = noahowpgrid%opt_rad(ix,iy)
-    noahowp%options%opt_sfc   = noahowpgrid%opt_sfc(ix,iy)
-    noahowp%options%opt_crs   = noahowpgrid%opt_crs(ix,iy)
-    noahowp%options%opt_crop  = noahowpgrid%opt_crop(ix,iy)
-    noahowp%options%opt_stc   = noahowpgrid%opt_stc(ix,iy)
-    noahowp%options%opt_tbot  = noahowpgrid%opt_tbot(ix,iy)
-    noahowp%options%opt_frz   = noahowpgrid%opt_frz(ix,iy)
-    noahowp%options%opt_btr   = noahowpgrid%opt_btr(ix,iy)
-    noahowp%options%opt_rsf   = noahowpgrid%opt_rsf(ix,iy)
-    noahowp%options%opt_sub   = noahowpgrid%opt_sub(ix,iy)
-          
-    end associate
+    options%opt_snf   = optionsgrid%opt_snf(ix,iy)
+    options%opt_run   = optionsgrid%opt_run(ix,iy)
+    options%opt_drn   = optionsgrid%opt_drn(ix,iy)
+    options%opt_inf   = optionsgrid%opt_inf(ix,iy)
+    options%opt_infdv = optionsgrid%opt_infdv(ix,iy)
+    options%dveg      = optionsgrid%dveg(ix,iy)
+    options%opt_alb   = optionsgrid%opt_alb(ix,iy)
+    options%opt_rad   = optionsgrid%opt_rad(ix,iy)
+    options%opt_sfc   = optionsgrid%opt_sfc(ix,iy)
+    options%opt_crs   = optionsgrid%opt_crs(ix,iy)
+    options%opt_crop  = optionsgrid%opt_crop(ix,iy)
+    options%opt_stc   = optionsgrid%opt_stc(ix,iy)
+    options%opt_tbot  = optionsgrid%opt_tbot(ix,iy)
+    options%opt_frz   = optionsgrid%opt_frz(ix,iy)
+    options%opt_btr   = optionsgrid%opt_btr(ix,iy)
+    options%opt_rsf   = optionsgrid%opt_rsf(ix,iy)
+    options%opt_sub   = optionsgrid%opt_sub(ix,iy)
 
   end subroutine OptionsVarInTransfer
 
-  subroutine OptionsVarOutTransfer(noahowp, noahowpgrid)
+  subroutine OptionsVarOutTransfer(noahowp, optionsgrid)
 
     implicit none
 
-    type(noahowpgrid_type), intent(inout) :: noahowpgrid
-    type(noahowp_type),     intent(in)    :: noahowp
-
-    associate(ix   => noahowpgrid%ix, &
-              iy   => noahowpgrid%iy)
+    type(options_type),         intent(in)       :: options
+    type(optionsgrid_type),     intent(inout)    :: optionsgrid
+    integer,                    intent(in)       :: ix
+    integer,                    intent(in)       :: iy
   
-    noahowpgrid%opt_snf(ix,iy) = noahowp%options%opt_snf
-    noahowpgrid%opt_run(ix,iy) = noahowp%options%opt_run
-    noahowpgrid%opt_drn(ix,iy) = noahowp%options%opt_drn
-    noahowpgrid%opt_inf(ix,iy) = noahowp%options%opt_inf
-    noahowpgrid%opt_infdv(ix,iy) = noahowp%options%opt_infdv
-    noahowpgrid%dveg(ix,iy) = noahowp%options%dveg
-    noahowpgrid%opt_alb(ix,iy) = noahowp%options%opt_alb
-    noahowpgrid%opt_rad(ix,iy) = noahowp%options%opt_rad
-    noahowpgrid%opt_sfc(ix,iy) = noahowp%options%opt_sfc
-    noahowpgrid%opt_crs(ix,iy) = noahowp%options%opt_crs
-    noahowpgrid%opt_crop(ix,iy) = noahowp%options%opt_crop
-    noahowpgrid%opt_stc(ix,iy) = noahowp%options%opt_stc
-    noahowpgrid%opt_tbot(ix,iy) = noahowp%options%opt_tbot
-    noahowpgrid%opt_frz(ix,iy) = noahowp%options%opt_frz
-    noahowpgrid%opt_btr(ix,iy) = noahowp%options%opt_btr
-    noahowpgrid%opt_rsf(ix,iy) = noahowp%options%opt_rsf
-    noahowpgrid%opt_sub(ix,iy) = noahowp%options%opt_sub
-          
-    end associate
+    ! Nothing to do
 
   end subroutine OptionsVarOutTransfer
 
