6772699368ecd557d4bca78a9f658f03f0eef954
GreyEvenson-NOAA <grey.evenson@noaa.gov>
2023-08-08 11:41:56 -0400

    Revised to vary lat, lon, and vegtyp across grid - though cell (1,1) will have lat, lon, and vegtyp of control case

--- test/analysis/create_dummy_netcdf.py
+++ test/analysis/create_dummy_netcdf.py
@@ -1,16 +1,16 @@
 import netCDF4 as nc
 import numpy as np
+import random
 
 # Define dataset contents
 n_y = 3
 n_x = 2
-dx = 100.0
-dy = 100.0
-nveg = 20
+dx = 30. # arc-seconds (~1 km)
+dy = 30.
 vegtyp = 1
 lat = 40.01
 lon = -88.37
-veg_class_name = "MODIFIED_IGBP_MODIS_NOAH"
+cnv_arcsec_to_degrees = 0.0002777777777777778
 
 # Create and open dataset
 loc = '../../data/netcdf_dummy.nc'
@@ -28,13 +28,15 @@ var_vegtyp = dum.createVariable('vegtyp', 'i4', ('Latitude','Longitude'))
 # Create attributes
 var_lon.dx = dx
 var_lat.dy = dy
-var_vegtyp.nveg = nveg
-var_vegtyp.veg_class_name = veg_class_name
 
 # Fill variables
-var_lat[:] = np.full(n_y, lat)  
-var_lon[:] = np.full(n_x, lon) 
+var_lat[:] = np.array([lat+(i*dy*cnv_arcsec_to_degrees) for i in range(n_y)])
+var_lon[:] = np.array([lon+(i*dx*cnv_arcsec_to_degrees) for i in range(n_x)]) 
 var_vegtyp[:,:] = np.full((n_y, n_x), vegtyp)
+for x in range(n_x):
+  for y in range(n_y):
+    if x != 0 or y != 0:
+      var_vegtyp[y,x] = random.randint(1,20)
 
 # Print some stuff
 print('\n****************DIMENSIONS****************')
