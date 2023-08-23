ba93adb8f603612409d8ca244807abd3781b7ecb
GreyEvenson-NOAA <grey.evenson@noaa.gov>
2023-08-23 16:33:14 -0400

    Cleanup

--- src/OptionsTypeTransfer.f90
+++ src/OptionsTypeTransfer.f90
@@ -7,39 +7,27 @@ module OptionsTypeTransfer
   
   contains
   
-  subroutine OptionsVarInTransfer(options, optionsgrid)
+  subroutine OptionsVarInTransfer(options, optionsgrid, ix, iy)
 
     implicit none
 
     type(options_type),         intent(inout)    :: options
     type(optionsgrid_type),     intent(in)       :: optionsgrid
+    integer,                    intent(in)       :: ix
+    integer,                    intent(in)       :: iy
   
-    options%opt_snf   = optionsgrid%opt_snf
-    options%opt_run   = optionsgrid%opt_run
-    options%opt_drn   = optionsgrid%opt_drn
-    options%opt_inf   = optionsgrid%opt_inf
-    options%opt_infdv = optionsgrid%opt_infdv
-    options%dveg      = optionsgrid%dveg
-    options%opt_alb   = optionsgrid%opt_alb
-    options%opt_rad   = optionsgrid%opt_rad
-    options%opt_sfc   = optionsgrid%opt_sfc
-    options%opt_crs   = optionsgrid%opt_crs
-    options%opt_crop  = optionsgrid%opt_crop
-    options%opt_stc   = optionsgrid%opt_stc
-    options%opt_tbot  = optionsgrid%opt_tbot
-    options%opt_frz   = optionsgrid%opt_frz
-    options%opt_btr   = optionsgrid%opt_btr
-    options%opt_rsf   = optionsgrid%opt_rsf
-    options%opt_sub   = optionsgrid%opt_sub
+    ! Nothing to do
 
   end subroutine OptionsVarInTransfer
 
-  subroutine OptionsVarOutTransfer(options, optionsgrid)
+  subroutine OptionsVarOutTransfer(options, optionsgrid, ix, iy)
 
     implicit none
 
     type(options_type),         intent(in)       :: options
     type(optionsgrid_type),     intent(inout)    :: optionsgrid
+    integer,                    intent(in)       :: ix
+    integer,                    intent(in)       :: iy
   
     ! Nothing to do
 
